151ab8951f7f02817060831f82c0f126b798d0d9
595520359 <595520359@qq.com>
2022-07-01 16:52:50 +0800

    4

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,76 @@ export default {
   data() {
     return {
       message: 'helloworld',
+      isActive: false,
+      isActiveA: false,
+      isActiveB: true,
+      testWatch: true,
+      pinkBackground: 'pink-background',
+      bindObj: {
+        id: 'one',
+        class: 'red-color',
+        style: {
+          fontSize: '4rem',
+          marginLeft: '2vw',
+        },
+      },
+      watchObj: {
+        name: '张三',
+        age: 26,
+        sex: '男',
+        isLog: true,
+      },
     }
   },
+  watch: {
+    // testWatch(newV, oldV) {
+    //   if (newV) {
+    //     this.isActive = true
+    //   }
+    // watchObj(newv, oldv) {
+    //   console.log('change!!!')
+    // },
+    // watchObj: {
+    //   handler(newv, oldv) {
+    //     console.log('change!!!')
+    //   },
+    //   deep: true,
+    // },
+    'watchObj.isLog'(newv, oldv) {
+      console.log('change!!!')
+    },
+  },
   methods: {},
-  computed: {},
+  computed: {
+    comIsActive() {
+      return this.isActiveA && this.isActiveB
+    },
+  },
 }
 </script>
 
 <template>
   <div>
-    <p>hello</p>
+    <!-- 通过对象绑定 -->
+    <p v-bind="bindObj" :class="{ [pinkBackground]: isActive }">
+      {{ message }}
+    </p>
+    <button @click="isActive = !isActive">改变</button>
+    <!-- 通过计算属性控制 -->
+    <p :class="{ [pinkBackground]: comIsActive }">
+      {{ message }}
+    </p>
+    <button @click="isActiveA = !isActiveA">改变A|{{ isActiveA }}</button>
+    <button @click="isActiveB = !isActiveB">改变B|{{ isActiveB }}</button>
+    <br />
+    <!-- 侦听器 -->
+    <button @click="testWatch = !testWatch">
+      改变侦听对象name|{{ testWatch }}
+    </button>
+    <br />
+    <button @click="watchObj.isLog = !watchObj.isLog">
+      改变侦听对象name|{{ watchObj.isLog }}
+    </button>
   </div>
 </template>
 
@@ -21,5 +81,14 @@ export default {
 
 body {
   margin-left: 2vw;
+  font-size: 1.5rem;
+}
+
+.red-color {
+  color: red;
+}
+
+.pink-background {
+  background-color: pink;
 }
 </style>
