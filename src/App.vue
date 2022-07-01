87e99e5db9a78a9a3ef4e58b082d2eed794dac9c
595520359 <595520359@qq.com>
2022-07-01 15:34:07 +0800

    1

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  data() {
+    return {
+      message: 'helloworld',
+    }
+  },
+  methods: {},
+  computed: {},
+}
+</script>
+
+<template>
+  <div>
+    <p>hello</p>
+  </div>
+</template>
+
+<style>
+@import './assets/base.css';
+
+body {
+  margin-left: 2vw;
+}
+</style>
